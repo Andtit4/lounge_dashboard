2c70f10eeee76dbe001b224a9d967e01eec5512e
Andtit4 <lirsitogo2021@gmail.com>
2025-05-13 10:05:10 +0000

    Refactor ecosystem.config.js for consistency and update database connection settings in app.module.ts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     apps: [{
-        name: "lounge-api",
-        cwd: "./backend",
-        script: "dist/main.js",
+        name: 'lounge-api',
+        cwd: './backend',
+        script: 'dist/main.js',
         instances: 1,
         autorestart: true,
         watch: false,
-        max_memory_restart: "1G",
+        max_memory_restart: '1G',
         env: {
-            NODE_ENV: "production",
-            PORT: 6610
-        }
-    }]
-};
+            NODE_ENV: 'production',
+            PORT: 6610,
+        },
+    }, ],
+}
